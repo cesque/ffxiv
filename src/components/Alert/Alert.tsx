69df06e0558840abb91124489c3c7ef1f5be7988
Daniel James <euqsec@gmail.com>
2026-06-15 01:38:00 +0100

    add Worqor Lar Dor

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -5,8 +5,9 @@ import { ReactNode } from 'react'
 import classNames from 'classnames'
 
 import WarningIcon from '@/icons/warning.svg'
+import InfoCircleIcon from '@/icons/info-circle.svg'
 
-type Icon = 'warning'
+type Icon = 'warning' | 'info-circle'
 
 interface Props {
     color: Color,
@@ -15,13 +16,18 @@ interface Props {
     children: ReactNode,
 }
 
+const icons: Record<Icon, ReactNode> = {
+    warning: <WarningIcon />,
+    'info-circle': <InfoCircleIcon />,
+}
+
 export default function Alert({ color = 'black', icon, title, children }: Props) {
     let classes = classNames(styles.alert, {
         [styles[`alert--${ color }`]]: true,
     })
 
     return <article className={ classes }>
-        <div className={ styles.icon }><WarningIcon /></div>
+        <div className={ styles.icon }>{ icons[icon] }</div>
         <div className={ styles.container }>
             { title && <header className={ styles.title }>{ title }</header> }
             <div className={ styles.content }>{ children }</div>
